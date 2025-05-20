acf07f3fd332fc1a531eb39fdf4288a520f7a766
Zack <168221270+Zval321@users.noreply.github.com>
2025-05-19 20:43:55 -0500

    added all calculator logic and ready for the design of the application

--- Calculator.js
+++ Calculator.js
@@ -1,3 +1,14 @@
+
+
+
+
+
+
+
+
+
+
+
 function add(a, b) { 
   return a + b
 }
@@ -13,3 +24,20 @@ function multiply(a, b) {
 function divide(a, b) { 
   return a / b
 }
+
+function operate(o, a, b) { 
+  if(o === '/'){ 
+    return divide(a, b)
+  } else if (o === '+'){ 
+    return add(a, b)
+  } else if (o === '-'){ 
+    return subtract(a, b)
+  } else if(o === '*'){ 
+    return multiply(a, b)
+  } else { 
+    return null
+  }
+}
+
+
+
